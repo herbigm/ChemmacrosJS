c9308b6cf5b1cbeb0383b5ed6476c9ffcbd273f9
Marcus Herbig <marcus.herbig@chemie.tu-freiberg.de>
2025-04-23 14:42:29 +0200

    upload data to github for the first time

--- README.md
+++ README.md
@@ -1,1 +1,136 @@
+<!--
+
+author:  Dr. Marcus Herbig
+email:   marcus.herbig@chemie.tu-freiberg.de
+
+version: 0.1
+
+comment: This is a plugin for including chemical fomulas into LiaScript. The syntax is similar to the LaTeX package chemmacros.
+
+link:      style_chemmacros.css
+
+script:    chemmacros.js
+
+@ch
+<script run-once modify="false">
+function chemmacros() {
+  if (!window.interpretChemmacros) {
+    setTimeout(chemmacros, 100)
+    return
+  }
+    send.lia("HTML: " +interpretChemmacros('@0'))
+}
+chemmacros()
+"LIA: wait"
+</script>
+@end
+
+@reaction
+<script run-once modify="false">
+function chemmacros() {
+  if (!window.interpretChemmacros) {
+    setTimeout(chemmacros, 100)
+    return
+  }
+    send.lia("HTML: <div class=\"chemreaction\">" +interpretChemmacros('@0') + "</div>")
+}
+chemmacros()
+"LIA: wait"
+</script>
+@end
+
+@reactions
+<script run-once modify="false">
+function chemmacros() {
+  if (!window.interpretChemmacros) {
+    setTimeout(chemmacros, 100)
+    return
+  }
+    send.lia("HTML: " +formatReactions(`@'0`))
+}
+chemmacros()
+"LIA: wait"
+</script>
+@end
+
+@ghspic
+<script run-once modify="false">
+function ghspic() {
+  if (!window.interpretChemmacros) {
+    setTimeout(ghspic, 100)
+    return
+  }
+  const text = '@0';
+  const whichOnes = text.split(/\s+/);
+  let ret = "";
+  for (const type of whichOnes) {
+      switch (type) {
+          case "explos":
+              ret += "![explosive](img/ghs/ghs01.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+          case "flame":
+              ret += "![flammable](img/ghs/ghs02.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+          case "flame-O":
+              ret += "![oxidizing](img/ghs/ghs03.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+          case "bottle":
+              ret += "![gas under pressure](img/ghs/ghs04.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+          case "acid":
+              ret += "![corrosive](img/ghs/ghs05.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+          case "skull":
+              ret += "![acutely toxic](img/ghs/ghs06.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+          case "exclam":
+              ret += "![irritating](img/ghs/ghs07.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+          case "health":
+              ret += "![harmful](img/ghs/ghs08.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+          case "aqpol":
+              ret += "![hazadrous for the environment](img/ghs/ghs09.svg)<!-- class=\"ghspic\"-->\n";
+              break;
+      }
+  }
+  send.lia("LIASCRIPT: " + ret);
+}
+ghspic()
+"LIA: wait"
+</script> 
+@end
+
+@signalword: <div class="signalword">@0</div>
+
+@statementsLangType
+<script run-once modify="false">
+async function hpstatements() {
+  if (!window.formatStatements) {
+    setTimeout(hpstatements, 100)
+    return
+  }
+  send.lia("HTML: " + await formatStatements('@0', '@1', `@'2`))
+}
+hpstatements()
+"LIA: wait"
+</script>
+@end
+
+@hstatements
+  @statementsLangType(en, H, @'0)
+@end
+
+@pstatements
+  @statementsLangType(en, P, @'0)
+@end
+-->
+
 # ChemmacrosJS
+This project aims to make the functions of the excellent LaTeX package *chemmacros* available for JavaScript and use in HTML. Integration in LiaScript (https://liascript.github.io/) is just as possible as use with vanilla JavaScript.
+
+## Use in Vanilla JavaScript
+
+
+## License
+This project is published to the terms of the [CC-BY Attribution 4.0 International Deed](https://creativecommons.org/licenses/by/4.0/).
---
+++ chemmacros.js
@@ -0,0 +1,263 @@
+async function INITchemmacros() {
+    const formulae = document.querySelectorAll(".chemmacros");
+    for (const f of formulae) {
+        let k = interpretChemmacros(f.innerText);
+        f.innerHTML = k;
+    }
+
+    const reaction = document.querySelectorAll(".chemreaction");
+    for (const f of reaction) {
+        let k = interpretChemmacros(f.innerText);
+        f.innerHTML = k;
+    }
+
+    const reactions = document.querySelectorAll(".chemreactions");
+    for (const f of reactions) {
+        f.style.display = "grid";
+        let equations = f.innerText.split("\\\\");
+        f.innerHTML = "";
+        for (const eqn of equations) {
+            let parts = eqn.split("&");
+            for (let i = 0; i < parts.length; i++) {
+                let span = document.createElement("span");
+                span.classList.add("eqnc" + i);
+                span.innerHTML = interpretChemmacros(parts[i]);
+                f.appendChild(span);
+            }
+            let span = document.createElement("span");
+            span.classList.add("eqncounter");
+            f.appendChild(span);
+        }
+    }
+
+    const ghspics = document.querySelectorAll(".ghspicContainer");
+    for (const f of ghspics) {
+        const whichOnes = f.dataset.pics.split(/\s+/);
+        for (const type of whichOnes) {
+            let pic = document.createElement("img");
+            pic.classList.add("ghspic");
+            switch (type) {
+                case "explos":
+                    pic.src = "img/ghs/ghs01.svg";
+                    f.appendChild(pic);
+                    break;
+                case "flame":
+                    pic.src = "img/ghs/ghs02.svg";
+                    f.appendChild(pic);
+                    break;
+                case "flame-O":
+                    pic.src = "img/ghs/ghs03.svg";
+                    f.appendChild(pic);
+                    break;
+                case "bottle":
+                    pic.src = "img/ghs/ghs04.svg";
+                    f.appendChild(pic);
+                    break;
+                case "acid":
+                    pic.src = "img/ghs/ghs05.svg";
+                    f.appendChild(pic);
+                    break;
+                case "skull":
+                    pic.src = "img/ghs/ghs06.svg";
+                    f.appendChild(pic);
+                    break;
+                case "exclam":
+                    pic.src = "img/ghs/ghs07.svg";
+                    f.appendChild(pic);
+                    break;
+                case "health":
+                    pic.src = "img/ghs/ghs08.svg";
+                    f.appendChild(pic);
+                    break;
+                case "aqpol":
+                    pic.src = "img/ghs/ghs09.svg";
+                    f.appendChild(pic);
+                    break;
+            }
+        }
+    }
+
+    const clp = document.querySelectorAll(".clpstatements");
+    for (const f of clp) {
+        f.style.display = "grid";
+
+        let lang = "en";
+        if (f.dataset.lang) {
+            lang = f.dataset.lang;
+        }
+        let res = await fetch("clp/hpstatements-"+lang+"-latest.json");
+        let all = await res.json();
+        console.log(all);
+        let value = f.dataset.hstatements;
+        if (value) {
+            let H = [];
+            let satzListe = value.matchAll(/((EUH|H|P)?\d{3}[ifdDF]*\s*\+?\s*){1,3}/g);
+            for (const satz of satzListe) {
+                let list = satz[0].match(/(\d{3}[ifdDF]*)/g);
+                if (satz[2] == "EUH" || list[0].startsWith("0")) {
+                value = "EUH" + list[0];
+                } else {
+                value = "H" + list.join("+H");
+                }
+                H.push(value);
+            }
+            H.sort();
+            H = [...new Set(H)];
+
+            for (const h of H) {
+                let dt = document.createElement("span");
+                dt.classList.add("clpDT");
+                dt.textContent = h;
+                f.appendChild(dt);
+                let dd = document.createElement("span");
+                dd.classList.add("clpDD");
+                dd.textContent = all["statements"]["latest/"+lang+"/" + h];
+                f.appendChild(dd);
+            }
+        }
+        value = null;
+        value = f.dataset.pstatements;
+        if (value) {
+            let P = [];
+            let satzListe = value.matchAll(/((EUH|H|P)?\d{3}[ifdDF]*\s*\+?\s*){1,3}/g);
+            for (const satz of satzListe) {
+                let list = satz[0].match(/(\d{3}[ifdDF]*)/g);
+                if (satz[2] == "EUH" || list[0].startsWith("0")) {
+                value = "EUH" + list[0];
+                } else {
+                value = "P" + list.join("+P");
+                }
+                P.push(value);
+            }
+            P.sort();
+            P = [...new Set(P)];
+
+            for (const p of P) {
+                let dt = document.createElement("span");
+                dt.classList.add("clpDT");
+                dt.textContent = p;
+                f.appendChild(dt);
+                let dd = document.createElement("span");
+                dd.classList.add("clpDD");
+                dd.textContent = all["statements"]["latest/"+lang+"/" + p];
+                f.appendChild(dd);
+            }
+        }
+    }
+}
+
+function interpretChemmacros(text) {
+    let parts = text.split(/\s+/gi);
+    for (let i = 0; i < parts.length; i++) {
+        parts[i] = parts[i].trim();
+        // replace . and * as middle dot
+        parts[i] = parts[i].replaceAll(/\s+[.*]\s+/gui, "&middot;");
+        // superscript with ^
+        parts[i] = parts[i].replaceAll(/\s*\^\{([\d\+\-\S]+)\}\s*/gui, "<sup class=\"chemmacrosSuper\">$1</sup>");
+        parts[i] = parts[i].replaceAll(/\s*\^([\d\+\-\S]{1})\s*/gui, "<sup class=\"chemmacrosSuper\">$1</sup>");
+        // superscript of charge
+        parts[i] = parts[i].replaceAll(/(\S)([+\-]){1}$/gui, "$1<sup class=\"chemmacrosSuper\">$2</sup>");
+        // numbers after element symbols
+        parts[i] = parts[i].replaceAll(/\s*([A-Z]{1}[a-z]?|\)|\})(\d+)\s*/gui, "$1<sub class=\"chemmacrosSub\">$2</sub>");
+        // subscript after _
+        parts[i] = parts[i].replaceAll(/_\{([^\{\}]+)\}/gui, "<sub class=\"chemmacrosSub\">$1</sub>");
+        parts[i] = parts[i].replaceAll(/_([^\{\}]{1})/gui, "<sub class=\"chemmacrosSub\">$1</sub>");
+
+        //arrows
+        parts[i] = parts[i].replaceAll(/(\\rightarrow|->)/gui, "&srarr;");
+        parts[i] = parts[i].replaceAll(/(\\uparrow|\^)/gui, "&uparrow;");
+        parts[i] = parts[i].replaceAll(/(\\downarrow|v)/gui, "&downarrow;");
+        parts[i] = parts[i].replaceAll(/(\\leftrightarrows|<=>)/gui, "&rlarr;");
+        // clean up
+        parts[i] = parts[i].replaceAll("</sup><sup class=\"chemmacrosSuper\">", "");
+        parts[i] = parts[i].replaceAll("</sub><sup class=\"chemmacrosSuper\">", "</sub><sup class=\"chemmacrosSuperBack\">");
+    }
+    text = parts.join(" ");
+    return text;
+}
+
+// equations
+let i = 0; // counter
+function formatReactions(text) {
+    let f = document.createElement("div");
+    f.classList.add("chemreactions");
+    let equations = text.split("\\\\");
+    for (const eqn of equations) {
+        let parts = eqn.split("&");
+        for (let i = 0; i < parts.length; i++) {
+            let span = document.createElement("span");
+            span.classList.add("eqnc" + i);
+            span.innerHTML = interpretChemmacros(parts[i]);
+            f.appendChild(span);
+        }
+        let span = document.createElement("span");
+        // counter
+        i++;
+        span.classList.add("eqncounter");
+        span.innerText = "(" + (i) + ")";
+        f.appendChild(span);
+    }
+    return f.outerHTML;
+}
+
+async function formatStatements (lang, t, text) {
+    let f = document.createElement("div");
+    f.classList.add("hpstatements");
+    let res = await fetch("https://raw.githubusercontent.com/mhchem/hpstatements/refs/heads/master/clp/hpstatements-"+lang+"-latest.json");
+    let all = await res.json();
+    let value = text;
+    t = t.trim();
+    if (t == 'H') {
+        let H = [];
+        let satzListe = value.matchAll(/((EUH|H|P)?\d{3}[ifdDF]*\s*\+?\s*){1,3}/g);
+        for (const satz of satzListe) {
+            let list = satz[0].match(/(\d{3}[ifdDF]*)/g);
+            if (satz[2] == "EUH" || list[0].startsWith("0")) {
+            value = "EUH" + list[0];
+            } else {
+            value = "H" + list.join("+H");
+            }
+            H.push(value);
+        }
+        H.sort();
+        H = [...new Set(H)];
+        for (const h of H) {
+            let dt = document.createElement("span");
+            dt.classList.add("clpDT");
+            dt.textContent = h;
+            f.appendChild(dt);
+            let dd = document.createElement("span");
+            dd.classList.add("clpDD");
+            dd.textContent = all["statements"]["latest/"+lang+"/" + h];
+            f.appendChild(dd);
+        }
+    } else if (t == "P") {
+        let P = [];
+        let satzListe = value.matchAll(/((EUH|H|P)?\d{3}[ifdDF]*\s*\+?\s*){1,3}/g);
+        for (const satz of satzListe) {
+            let list = satz[0].match(/(\d{3}[ifdDF]*)/g);
+            if (satz[2] == "EUH" || list[0].startsWith("0")) {
+            value = "EUH" + list[0];
+            } else {
+            value = "P" + list.join("+P");
+            }
+            P.push(value);
+        }
+        P.sort();
+        P = [...new Set(P)];
+
+        for (const p of P) {
+            let dt = document.createElement("span");
+            dt.classList.add("clpDT");
+            dt.textContent = p;
+            f.appendChild(dt);
+            let dd = document.createElement("span");
+            dd.classList.add("clpDD");
+            dd.textContent = all["statements"]["latest/"+lang+"/" + p];
+            f.appendChild(dd);
+        }
+    } else {
+        console.log("Hä!?!");
+    }
+    return f.outerHTML;
+}
